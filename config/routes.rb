37ee75a43e3656afdfb39636fbd0cd0ccc0bcff1
Euclides da Costa Rego Neto <euclides.neto@msn.com>
2024-11-09 15:46:12 +1030

    implemented admin feature

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,15 @@
 Rails.application.routes.draw do
-  get 'pages/about'
-  get 'pages/contact'
-  get 'pages/schedule'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
-  #
-  #Rails.application.routes.draw do
+  # Public pages (no authentication needed)
   root 'pages#about'
-
   get 'about', to: 'pages#about'
   get 'contact', to: 'pages#contact'
   get 'schedule', to: 'pages#schedule'
 
+  # Devise routes for login, sign up, etc.
+  devise_for :users
 
+  # Admin routes (requires authentication and admin privileges)
+  namespace :admin do
+    get 'dashboard', to: 'admin#dashboard'
+  end
 end
