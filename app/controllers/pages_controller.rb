afe7f4fe32f68c0ef3ff82bf7d1863b164f8c63a
Euclides da Costa Rego Neto <euclides.neto@msn.com>
2025-02-28 19:15:13 +1030

    Updated color scheme and implemented contact page with form submission functionality

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,8 +3,23 @@ class PagesController < ApplicationController
   end
 
   def contact
+    if request.post?
+      # You can either save the form data to the database or send an email
+      name = params[:name]
+      email = params[:email]
+      message = params[:message]
+
+      ContactMailer.contact_email(name, email, message).deliver_now
+
+      # Example: Send email using ActionMailer (assuming you have a Mailer set up)
+      # ContactMailer.contact_email(name, email, message).deliver_now
+
+      flash[:notice] = "Thank you for contacting us. We'll get back to you soon."
+      redirect_to contact_path
+    end
   end
 
   def schedule
   end
+
 end
