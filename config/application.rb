c01d49c03b2db3c0c7f30fe54c00d3543ad93abf
Euclides da Costa Rego Neto <euclides.neto@msn.com>
2025-05-25 15:40:44 +0930

    adjusted the time to be updated to 8pm and add date to the banner

--- config/application.rb
+++ config/application.rb
@@ -30,7 +30,10 @@ module LightAndPeace
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
-
+    config.time_zone = 'Adelaide'
+    config.active_record.default_timezone = :local
+    config.active_record.time_zone_aware_types = [:datetime, :time]
+    config.generators.system_tests = nil
     # Don't generate system test files.
     config.generators.system_tests = nil
   end
