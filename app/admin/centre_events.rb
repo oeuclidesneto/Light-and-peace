0f0bf2868f5252c6849323ce1c0d4ceada661ec8
Euclides da Costa Rego Neto <euclides.neto@msn.com>
2025-05-25 17:34:29 +0930

    Improved Events page: dynamic listing, empty state design, and CMS date input enhancements

--- app/admin/centre_events.rb
+++ app/admin/centre_events.rb
@@ -1,19 +1,6 @@
 ActiveAdmin.register CentreEvent do
   menu label: "📅 Centre Events", priority: 3
-  # See permitted parameters documentation:
-  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
-  # Uncomment all parameters which should be permitted for assignment
-  #
-  # permit_params :title, :date, :description, :location, :icon
-  #
-  # or
-  #
-  # permit_params do
-  #   permitted = [:title, :date, :description, :location, :icon]
-  #   permitted << :other if params[:action] == 'create' && current_user.admin?
-  #   permitted
-  # end
+
   permit_params :title, :date, :description, :location, :icon
 
   ICON_OPTIONS = {
@@ -27,11 +14,35 @@ ActiveAdmin.register CentreEvent do
   form do |f|
     f.inputs "Event Details" do
       f.input :title
-      f.input :date
-      f.input :location
       f.input :description
-      f.input :icon, as: :select, collection: ICON_OPTIONS.invert, include_blank: false
+      f.input :location
+      f.input :icon, input_html: { value: 'fa-solid fa-calendar-days' }
+
+      # Separate date and time fields
+      f.input :event_date, as: :string, label: "Date (dd/mm/yyyy)", input_html: { value: f.object.date&.strftime("%d/%m/%Y") }
+      f.input :event_time, as: :string, label: "Time (HH:MM)", input_html: { value: f.object.date&.strftime("%H:%M") }
     end
     f.actions
+  end
+
+  controller do
+    def update
+      params[:centre_event][:date] = parse_datetime(params[:centre_event].delete(:event_date), params[:centre_event].delete(:event_time))
+      super
+    end
+
+    def create
+      params[:centre_event][:date] = parse_datetime(params[:centre_event].delete(:event_date), params[:centre_event].delete(:event_time))
+      super
+    end
+
+    private
+
+    def parse_datetime(date_str, time_str)
+      return nil if date_str.blank? || time_str.blank?
+      Time.strptime("#{date_str} #{time_str}", "%d/%m/%Y %H:%M")
+    rescue
+      nil
+    end
   end
 end
