4cb02810665594c02571cb66a63ff5b29f163dc9
Euclides da Costa Rego Neto <euclides.neto@msn.com>
2025-08-10 23:41:37 +0930

    Switch to Gmail SMTP for testing

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,14 +58,15 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    address:              "smtp.sendgrid.net",
-    port:                 587,
-    domain:               ENV.fetch("MAILER_DOMAIN", "light-and-peace.herokuapp.com"),
-    user_name:            "apikey", # literal string for SendGrid API keys
-    password:             ENV.fetch("SENDGRID_API_KEY"),
-    authentication:       :plain,
-    enable_starttls_auto: true
-  }
+  address:              "smtp.gmail.com",
+  port:                 587,
+  domain:               "gmail.com",
+  user_name:            ENV["GMAIL_USERNAME"],
+  password:             ENV["GMAIL_APP_PASSWORD"],
+  authentication:       :plain,
+  enable_starttls_auto: true
+}
+
   # ---------------------------------------------
 
   config.i18n.fallbacks = true
